21156fc60263827865696f22193164346148a8ab
Igor Kuzmenko <kuzzmi@gmail.com>
2015-11-16 17:10:28 +0100

    Fixed issue with link-to

--- app/router.js
+++ app/router.js
@@ -2,13 +2,13 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
-  this.route('all-posts', function() {
-      this.route('post-new');
-  });
+    this.route('all-posts', { path: '/posts' }, function() {
+        this.route('post-new', { path: '/new' });
+    });
 });
 
 export default Router;
