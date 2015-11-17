9195e42c174bcab3655efb72edbc2dd801771a77
Igor Kuzmenko <kuzzmi@gmail.com>
2015-11-17 12:55:55 +0100

    Added oauth2 authenticator

---
+++ client/app/controllers/auth/login.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    session: Ember.inject.service('session'),
+
+    actions: {
+        authenticate() {
+            let { username, password } = this.getProperties('username', 'password');
+
+            this.get('session').authenticate('authenticator:oauth2', username, password)
+            .catch((reason) => {
+                this.set('errorMessage', reason.error || reason);
+            })
+        }
+    }
+});
